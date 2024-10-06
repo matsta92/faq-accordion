2d85af010af47f3af6dfbf564796e635ca0f8ba5
matsta92~ <matsta92@gmail.com>
2024-10-06 15:48:42 +0200

    update links

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ faqs.forEach((faq) => {
 
 		if (answer.style.display === 'block') {
 			answer.style.display = 'none';
-			icon.src = 'assets/images/icon-plus.svg';
+			icon.src = './assets/images/icon-plus.svg';
 		} else {
 			answer.style.display = 'block';
-			icon.src = 'assets/images/icon-minus.svg';
+			icon.src = './assets/images/icon-minus.svg';
 		}
 	});
 });
